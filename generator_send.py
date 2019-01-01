792b4e630db0bff9f87428b8f80b523c026e134b
李大王 <lijiaming@lidawangdeMacBook-Air.local>
2019-01-01 18:44:41 +0800

    生成器中的一个send的方法
    生成器对象，send(value)
    和next一样搞一次循环  但是它可以传值
    但是最好不要把他第一次循环
    除非传None 不然不好接受值

---
+++ generator_send.py
@@ -0,0 +1,42 @@
+def fibonacci(all_len):
+    current_position=0
+    a, b=0, 1
+
+    while current_position < all_len:
+        ret=yield a #!!!
+        print('>>>ret>>>',ret)
+        a ,b=b, a+b
+        current_position +=1
+    return 'ok'
+
+
+obj=fibonacci(10)#like a  class
+
+
+ret=next(obj)
+print(ret)
+
+#or
+# obj.send(None)
+
+
+
+
+ret=obj.send('hahah')
+print(ret)
+
+ret=obj.send('ah')
+print(ret)
+
+ret=obj.send('ddah')
+print(ret)
+
+
+'''>>>print'''
+# 0
+# >>>ret>>> hahah
+# 1
+# >>>ret>>> ah
+# 1
+# >>>ret>>> ddah
+# 2
