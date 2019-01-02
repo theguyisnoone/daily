080d27828616e6d2c44f564773df5f10450272e7
李大王 <lijiaming@lidawangdeMacBook-Air.local>
2019-01-02 14:25:17 +0800

    bug
    url地址有问题

--- downloadvideo.py
+++ downloadvideo.py
@@ -16,7 +16,7 @@ def my_download(filename,url):
     print('{} bytes received from {} '.format(len(data),url))
 
 gevent.joinall([
-gevent.spawn(my_download,1.jpg,'http:\u002F\u002Fi1.hdslb.com\u002Fbfs\u002Farchive\u002Fb9e8630d22c17f047ba9f3ea2a2c426070f4d9fe.jpg'),
-gevent.spawn(my_download,2.png,'http:\u002F\u002Fi0.hdslb.com\u002Fbfs\u002Farchive\u002F047cd6057bdfe75c818c08d0b3563afca46f7c5a.png'),
+gevent.spawn(my_download,'1.mp4','https://www.bilibili.com/video/av1665556/'),
+gevent.spawn(my_download,'2.mp4','https://www.bilibili.com/video/av1665556/'),
 
 ])
