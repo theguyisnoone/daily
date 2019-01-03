cb371a1bc3a12f1978e8b9c7afa4f6ebf8f440a0
李大王 <lijiaming@lidawangdeMacBook-Air.local>
2019-01-03 10:35:17 +0800

    crud

--- mongo/mongopie.py
+++ mongo/mongopie.py
@@ -6,6 +6,35 @@ client=MongoClient(host='localhost',port=27017)
 db=client['lee']
 collection=db['jaja']
 
-
-result=collection.insert({'_id':'1','name':'lee'})
+# #insert one
+result=collection.insert({'_id':'2','name':'le'})
 print(result)
+
+# #insert many
+item_list=[{'name':'test{}'.format(i)} for i in range(10)]
+t=collection.insert_many(item_list)
+for i in t.inserted_ids:
+    print(i)
+
+#find_one
+t=collection.find_one({'name':'test5'})
+print(t)
+
+#find_many
+t=collection.find({'name':'test5'})
+for i in t:
+    print(i)
+for i in t:
+    print(i)
+
+#update_one
+collection.update_one({'name':'test6'},{"$set":{'name':'new_test6'}})
+
+#update_many
+collection.update_many({'name':'test1'},{'$set':{'name':'new_test1')})
+
+#delete one
+collection.delete_one({'name':'new_test9'})
+
+#delete many
+collection.delete_many({'name':'new_test9'})
